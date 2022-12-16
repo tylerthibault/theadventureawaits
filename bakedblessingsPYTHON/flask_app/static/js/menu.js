0941bd1a2e3cca736565d1e26232768465207e3a
Tyler Thibault <tyler.thibault@protonmail.com>
2022-12-16 02:26:27 -0700

    update on menu

--- bakedblessingsPYTHON/flask_app/static/js/menu.js
+++ bakedblessingsPYTHON/flask_app/static/js/menu.js
@@ -1,16 +1,28 @@
 var productBtns = document.querySelectorAll('.product-btn')
 
 for (let btn of productBtns) {
-    btn.addEventListener("click", function(){
+    btn.addEventListener("click", function () {
         toggleMenuDisplay(btn)
     })
 }
 
-function toggleMenuDisplay(el){
+function toggleMenuDisplay(el) {
     let targetName = el.getAttribute("target")
+
+    for (let btn of productBtns) {
+        if (btn === el) {
+            btn.classList.remove("bg-white")
+            btn.classList.add("bg-bb-color-a", "shadow-white")
+        } else {
+            btn.classList.add("bg-white")
+            btn.classList.remove("bg-bb-color-a", "shadow-white")
+        }
+    }
+
     const allTargets = document.querySelectorAll('.product-display')
+    console.log(allTargets)
     for (const item of allTargets) {
-        if (item.getAttribute("id") != targetName){
+        if (item.getAttribute("id") != targetName) {
             item.classList.add("hidden")
         } else {
             item.classList.remove("hidden")
